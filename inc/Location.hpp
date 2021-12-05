ff5c0d22845451894c62c7dd740b2257202b6e14
Achopper <73693159+Achopper@users.noreply.github.com>
2021-12-05 13:20:15 +0300

    Update Location.hpp

--- inc/Location.hpp
+++ inc/Location.hpp
@@ -7,34 +7,34 @@
 class Location {
 private:
 
-	std::string 								_path;
-	std::map<std::string, bool> 				_methods;
-	size_t 										_clientMaxBodySize;
-	std::string 								_index;
-	std::string									_root;
-	bool 										_autoindex;
+	std::string 							_path;
+	std::map<std::string, bool> 					_methods;
+	size_t 								_clientMaxBodySize;
+	std::string 							_index;
+	std::string							_root;
+	bool 								_autoindex;
 
 public:
-	Location									( void );
-	Location									( Location const & obj );
-	~Location									( void );
+	Location							( void );
+	Location							( Location const & obj );
+	~Location							( void );
 
-	Location & 									operator=( Location const & obj );
+	Location & 							operator=( Location const & obj );
 
 public:
 
-	bool		setPath							( std::string const & path );
+	bool		setPath						( std::string const & path );
 	bool 		setAutoindex					( std::string const & autoindex );
-	bool		setMethods						( std::string const & methods );
+	bool		setMethods					( std::string const & methods );
 	bool 		setClientBodySize				( std::string const & num );
-	bool		setIndex						( std::string const & index );
-	bool 		setRoot							( std::string const & root );
+	bool		setIndex					( std::string const & index );
+	bool 		setRoot						( std::string const & root );
 
 	bool 		getAutoindex					( void ) const;
-	std::string getPath							( void ) const;
+	std::string getPath						( void ) const;
 	std::string getIndex						( void ) const;
-	std::string getRoot							( void ) const;
-	std::map<std::string, bool>	getMethods		( void ) const;
+	std::string getRoot						( void ) const;
+	std::map<std::string, bool>	getMethods			( void ) const;
 	size_t 		getClientSize					( void ) const;
 
 };
